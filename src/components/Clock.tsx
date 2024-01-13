9b79b0cfafff87927cb1da1ddce82bdea589fcb9
Yury Vereskun <satriks@mail.ru>
2024-01-13 23:51:29 +0300

    done task 1

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,6 +1,7 @@
 import { Moment } from "moment"
 import dial from "../assets/clock2.png"
 import arrow from "../assets/min.png" 
+import sec from "../assets/sec.png" 
 import { SetStateAction, useEffect } from "react"
 
 interface Props {
@@ -22,9 +23,9 @@ export default function Clock({watches, setWatches}: Props){
             const currentWatches = [...watches]
 
             const timeout = setTimeout(()=> {
-                currentWatches.forEach(el => el.time.add(1, "m"))
+                currentWatches.forEach(el => el.time.add(1, "s"))
                 setWatches(currentWatches)
-                }, 1000 * 60 )   
+                }, 1000  )   
 
             
             return ( () => window.clearTimeout(timeout))},
@@ -44,6 +45,7 @@ export default function Clock({watches, setWatches}: Props){
             <div className="clock" key={el.id} id={el.id}>
                 <h2 className="clock__name">{el.name}</h2>
                 <img src={dial}/>
+                <img src={sec} className="clock__second" style={{transform: `rotate(${(el.time.second() - (watches.length - 1 ))*6 + "deg"})`}}/>
                 <img src={arrow} className="clock__minute" style={{transform: `rotate(${el.time.minute()*6 + "deg"})`}}/>
                 <img src={arrow} className="clock__hour" style={{transform: `rotate(${el.time.hour()*30 + "deg"})`}}/>
                 <button 
